291e9e0e15995b95b1d4e321f4e401c5cc55b711
Hayden Ploszay <123116599+nedyah212@users.noreply.github.com>
2025-10-20 19:20:47 -0500

    I built a good chunk of DepthFirstSearch(), not tested yet

--- Maze.cs
+++ Maze.cs
@@ -72,17 +72,86 @@ namespace assignment_2_stack
 		{
 			Point currentPosition = StartingPoint;
 			Path.Push(currentPosition);
-			CharMaze[currentPosition.Row][currentPosition.Column] = 'V';
-			
+			SetChar(Path.Top(), 'V');
+			int count;
+
 			while (!Path.IsEmpty() == false)
+			{
+				count = 0;
 				currentPosition = Path.Top();
-				return "string";
-								
+
+				//Check left
+				if (IsSpace(InspectLocation(0, -1)))
+				{
+					Point point = new Point(Path.Top().Row, Path.Top().Column - 1);
+					Path.Push(point);
+					SetChar(Path.Top(), 'V');
+				}
+				else count += 1;
+
+				//Check right
+				if (IsSpace(InspectLocation(0, 1)))
+				{
+					Point point = new Point(Path.Top().Row, Path.Top().Column - 1);
+					Path.Push(point);
+					SetChar(Path.Top(), 'V');
+				}
+				else count += 1;
+
+				//Check down
+				if (IsSpace(InspectLocation(1, 0)))
+				{
+					Point point = new Point(Path.Top().Row, Path.Top().Column - 1);
+					Path.Push(point);
+					SetChar(Path.Top(), 'V');
+				}
+				else count += 1;
+
+				//Check up
+				if (IsSpace(InspectLocation(-1, 0)))
+				{
+					Point point = new Point(Path.Top().Row, Path.Top().Column - 1);
+					Path.Push(point);
+					SetChar(Path.Top(), 'V');
+				}
+				else count += 1;
+
+				if (count == 3)
+				{
+					Path.Pop();
+				}
+			}
+			
+			return "string";
 		}
 
 		public Stack<Point> GetPathToFollow()
 		{
 			throw new ApplicationException();
+		}
+
+		//Points towards a location, checking what the 
+		private char InspectLocation(int rowModifier, int columnModifier)
+		{	
+			char character = '-';
+			if (rowModifier != 0)
+			{
+				character = CharMaze[Path.Top().Row + rowModifier][Path.Top().Column];
+			}
+			if (columnModifier != 0)
+			{
+				character = CharMaze[Path.Top().Row][Path.Top().Column + columnModifier];
+			}
+			return character;
+		}
+
+		//Returns true if param is a ' '
+		private static bool IsSpace(char target) => target == ' ';
+
+		//Sets the char at a given position to a specific char
+		private void SetChar(Point position, Char character)
+		{
+			CharMaze[position.Row][position.Column] = character;
 		}
 	}
 }
